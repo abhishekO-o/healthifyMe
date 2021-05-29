d0b90f0237febb5831610e035bf5081b44029933
Abhishek Sharma <abhisheksharma.22091999@gmail.com>
2021-05-30 00:21:59 +0530

    first commit

---
+++ app/src/main/java/com/wolfie/checkingin/Database.java
@@ -0,0 +1,71 @@
+package com.wolfie.checkingin;
+
+import java.util.HashMap;
+
+public class Database {
+    public  HashMap<String, Integer> foodMap = new HashMap<>();
+
+    Database() {
+        foodMap.put("APPLE", 95);
+        foodMap.put("BANANA", 111);
+        foodMap.put("BLACKBERRIES", 62);
+        foodMap.put("CHERRIES", 4);
+        foodMap.put("GRAPES", 104);
+        foodMap.put("GUAVA", 37);
+        foodMap.put("LEMON", 17);
+        foodMap.put("MANGO", 202);
+        foodMap.put("ORANGE", 62);
+        foodMap.put("PAPAYA", 215);
+        foodMap.put("PINEAPPLE", 453);
+        foodMap.put("STRAWBERRIES", 49);
+        foodMap.put("WATERMELON", 86);
+        foodMap.put("DAL", 304);
+        foodMap.put("DOSA", 287);
+        foodMap.put("CARROT", 25);
+        foodMap.put("BROCCOLI", 207);
+        foodMap.put("CORN", 572);
+        foodMap.put("ONION", 34);
+        foodMap.put("TOMATO", 20);
+        foodMap.put("CARROT SOUP", 95);
+        foodMap.put("ONION SOUP", 56);
+        foodMap.put("TOMATO SOUP", 74);
+        foodMap.put("VEGETABLE SOUP", 67);
+        foodMap.put("COKE", 149);
+        foodMap.put("FANTA", 138);
+        foodMap.put("SODA", 188);
+        foodMap.put("BUTTERMILK", 157);
+        foodMap.put("CREAM", 36);
+        foodMap.put("CURD", 206);
+        foodMap.put("LASSI", 150);
+        foodMap.put("MILK", 149);
+        foodMap.put("YOGURT", 138);
+        foodMap.put("CORN WAFFLES", 110);
+        foodMap.put("WHOLEGRAIN OATS", 300);
+        foodMap.put("WHOLEGRAIN WHEAT", 407);
+        foodMap.put("TORTILLA", 467);
+        foodMap.put("CUPCAKE", 131);
+        foodMap.put("FRUIT CAKE", 139);
+        foodMap.put("PANCAKE", 86);
+        foodMap.put("WAFFLES", 103);
+        foodMap.put("CORN FLAKES", 130);
+        foodMap.put("TOASTIES", 134);
+        foodMap.put("RICE", 206);
+        foodMap.put("DRY VEGETABLE DISH", 150);
+        foodMap.put("COCONUT WATER", 15);
+        foodMap.put("CHEESE", 264);
+        foodMap.put("OIL", 920);
+        foodMap.put("GHEE", 920);
+        foodMap.put("BUTTER", 717);
+        foodMap.put("SUGGERCANE JUICE", 57);
+        foodMap.put("GREEN CHILLIES", 45);
+        foodMap.put("DATES DRY", 320);
+        foodMap.put("SPINACH", 24);
+        foodMap.put("LETTUCE", 21);
+        foodMap.put("PEAS", 303);
+        foodMap.put("RAJMA", 299);
+        foodMap.put("SOYA BEAN", 381);
+        foodMap.put("EGG", 134);
+        foodMap.put("CHICKEN", 383);
+        foodMap.put("LADIES FINGER", 27);
+    }
+}
